c142fed630a1e9935a771bf0b80232df3c8daff7
Johnny Chiu <chakpang01@gmail.com>
2021-02-12 01:39:53 +0800

    hello by johnny

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>hello !</h2>
+    </div>
+  );
 }
 
 export default App;
