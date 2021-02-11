951d2026666b98ff4668a69f9aed37750cb67f6b
honchunkin <honchunkin@gmail.com>
2021-02-12 01:38:53 +0800

    Helloworld by dick

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>Helloworld by dick</h1>    
+  </div>;
 }
 
 export default App;
