2bb89f1bbe120913012a9ec6f0d9bfbbb57e462d
honchunkin <honchunkin@gmail.com>
2021-02-12 02:35:15 +0800

    bugfix- height is not 100%

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,39 @@
 import React, { useState, useEffect } from "react";
+import Button from '@material-ui/core/Button';
+import "./Sidebar.css";
 
 export default function sidebar() {
-  return <div></div>;
+  return <div className="sideBarContainer">
+    <div className="flexRow">
+      <div className="flexCol sideBar">
+        <div className="sideBarTitle">
+          <h3>SideBar</h3>
+        </div>
+        <div className="flexCol sideBarButtonContainer">
+          <div className="sideBarButton">
+          <Button  variant="outlined" color="primary">
+            Button 1
+          </Button>
+          </div>
+          <div className="sideBarButton">
+          <Button  variant="outlined" color="primary">
+            Button 2
+          </Button>
+          </div>
+          <div className="sideBarButton">
+          <Button  variant="outlined" color="primary">
+            Button 3
+          </Button>
+          </div>
+          
+        </div>
+
+      </div>
+      <div className="content">
+        <h3 style={{textAlign: 'center'}}>This is content</h3>
+      </div>
+
+    </div>
+
+  </div>;
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SideBar from './components/Sidebar/Sidebar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SideBar/>
   </React.StrictMode>,
   document.getElementById('root')
 );
