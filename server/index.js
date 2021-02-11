e9c4c8eb7cc17941f398bd69a68cc969227da1c4
Johnny Chiu <chakpang01@gmail.com>
2021-02-12 01:36:55 +0800

    init commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const Twit = require("twit");
+
+require("dotenv").config();
+
+const T = new Twit({
+  consumer_key: process.env.TWITTER_API_key,
+  consumer_secret: process.env.TWITTER_API_secret_key,
+  access_token: process.env.TWITTER_API_access_token,
+  access_token_secret: process.env.TWITTER_API_acccess_token_secret,
+});
+
+(async = () => {
+  //   T.get("search/tweets", { q: "$TSLA", count: 5 }, (err, data, response) => {
+  //     const tweets = data.statuses.map((tweet) => tweet.text);
+  //     console.log(tweets);
+  //   });
+
+  var stream = T.stream("statuses/filter", { track: "$CCIV" });
+
+  stream.on("tweet", (tweet) => {
+    console.log(tweet.text);
+    console.log("---------");
+  });
+})();
