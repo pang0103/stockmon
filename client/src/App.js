fb41e0f2010ad5975b40cc5aaa395ebd1c4b860f
Johnny Chiu <chakpang01@gmail.com>
2021-02-12 16:35:32 +0800

    init commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Button } from "@material-ui/core";
+//import SideBar from "./components/Sidebar/Sidebar";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Button color="primary">Hello World</Button>
+      <Dashboard></Dashboard>
     </div>
   );
 }
